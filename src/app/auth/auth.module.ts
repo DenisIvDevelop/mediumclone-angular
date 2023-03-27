163045c9287e2e4a4fe2118ae47d15e219e88f6a
Denis Ivanov <ltybc3528@mail.ru>
2023-03-27 18:27:28 +0300

    add reactive forms in register component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { ReqisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
@@ -12,6 +14,6 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [ReqisterComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
 })
 export class AuthModule {}
