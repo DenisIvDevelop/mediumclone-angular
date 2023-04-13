a2e50eed3731c56f1abc64575c7aa244d37dd536
Denis Ivanov <ltybc3528@mail.ru>
2023-04-14 00:26:53 +0300

    add isSubmitting selector in register component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
+import { isSubmittingSelector } from './../../store/selectors';
 import { registerAction } from './../../store/actions/register.action';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'mc-register',
@@ -10,11 +12,17 @@ import { Store } from '@ngrx/store';
 })
 export class ReqisterComponent implements OnInit {
   form!: FormGroup;
+  isSubmitting$!: Observable<boolean>;
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
   }
 
   initializeForm(): void {
